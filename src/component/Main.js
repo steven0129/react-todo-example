25f567e12e842115ef2aaf6b9fd51f2d1c4981da
Yu-Hsin Lu <kerol2r20@gmail.com>
2018-05-03 23:19:29 +0800

    獨立 ListItem 為 stateless 組件，做到更加彈性的效果

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,33 +1,30 @@
-import React from 'react'
-import './Main.css'
+import React from "react";
+import "./Main.css";
+
+const TodoItem = props => (
+  <li className={props.completed ? props.completed : ""}>
+    <div className="view">
+      <input className="toggle" type="checkbox" checked={props.completed} />
+      <label>{props.content}</label>
+      <button className="destroy" />
+    </div>
+    <input className="edit" value="Create a TodoMVC template" />
+  </li>
+);
 
 class Main extends React.Component {
-    render() {
-        return (
-            <section className="main">
-				<input className="toggle-all" type="checkbox" />
-				<label for="toggle-all">Mark all as complete</label>
-				<ul className="todo-list">
-					<li className="completed">
-						<div className="view">
-							<input className="toggle" type="checkbox" checked />
-							<label>Taste JavaScript</label>
-							<button className="destroy"></button>
-						</div>
-						<input className="edit" value="Create a TodoMVC template" />
-					</li>
-					<li>
-						<div className="view">
-							<input className="toggle" type="checkbox" />
-							<label>Buy a unicorn</label>
-							<button classNasme="destroy"></button>
-						</div>
-						<input className="edit" value="Rule the web" />
-					</li>
-				</ul>
-			</section>
-        )
-    }
+  render() {
+    return (
+      <section className="main">
+        <input className="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul className="todo-list">
+          <TodoItem completed={true} content="Taste JavaScript" />
+          <TodoItem completed={false} content="Buy a unicorn" />
+        </ul>
+      </section>
+    );
+  }
 }
 
-export default Main
+export default Main;
